9cceea65cc8784a2a017fba3d14172ddaba3a3c3
Marcelo Perini <marceloprni@gmail.com>
2026-03-11 22:21:51 -0300

    vr110320261021

--- dist/desafios/interfaces/desafio.schema.d.ts
+++ dist/desafios/interfaces/desafio.schema.d.ts
@@ -4,21 +4,21 @@ export declare const DesafioSchema: mongoose.Schema<any, mongoose.Model<any, any
     collection: string;
 }, {
     jogadores: mongoose.Types.ObjectId[];
+    categoria?: string | null | undefined;
     dataHoraDesafio?: NativeDate | null | undefined;
+    solicitante?: mongoose.Types.ObjectId | null | undefined;
     status?: string | null | undefined;
     dataHoraSolicitacao?: NativeDate | null | undefined;
     dataHoraResposta?: NativeDate | null | undefined;
-    solicitante?: mongoose.Types.ObjectId | null | undefined;
-    categoria?: string | null | undefined;
     partida?: mongoose.Types.ObjectId | null | undefined;
 } & mongoose.DefaultTimestampProps, mongoose.Document<unknown, {}, {
     jogadores: mongoose.Types.ObjectId[];
+    categoria?: string | null | undefined;
     dataHoraDesafio?: NativeDate | null | undefined;
+    solicitante?: mongoose.Types.ObjectId | null | undefined;
     status?: string | null | undefined;
     dataHoraSolicitacao?: NativeDate | null | undefined;
     dataHoraResposta?: NativeDate | null | undefined;
-    solicitante?: mongoose.Types.ObjectId | null | undefined;
-    categoria?: string | null | undefined;
     partida?: mongoose.Types.ObjectId | null | undefined;
 } & mongoose.DefaultTimestampProps, {
     id: string;
@@ -27,12 +27,12 @@ export declare const DesafioSchema: mongoose.Schema<any, mongoose.Model<any, any
     collection: string;
 }>> & Omit<{
     jogadores: mongoose.Types.ObjectId[];
+    categoria?: string | null | undefined;
     dataHoraDesafio?: NativeDate | null | undefined;
+    solicitante?: mongoose.Types.ObjectId | null | undefined;
     status?: string | null | undefined;
     dataHoraSolicitacao?: NativeDate | null | undefined;
     dataHoraResposta?: NativeDate | null | undefined;
-    solicitante?: mongoose.Types.ObjectId | null | undefined;
-    categoria?: string | null | undefined;
     partida?: mongoose.Types.ObjectId | null | undefined;
 } & mongoose.DefaultTimestampProps & {
     _id: mongoose.Types.ObjectId;
@@ -45,12 +45,12 @@ export declare const DesafioSchema: mongoose.Schema<any, mongoose.Model<any, any
 } | {
     [x: string]: mongoose.SchemaDefinitionProperty<any, any, mongoose.Document<unknown, {}, {
         jogadores: mongoose.Types.ObjectId[];
+        categoria?: string | null | undefined;
         dataHoraDesafio?: NativeDate | null | undefined;
+        solicitante?: mongoose.Types.ObjectId | null | undefined;
         status?: string | null | undefined;
         dataHoraSolicitacao?: NativeDate | null | undefined;
         dataHoraResposta?: NativeDate | null | undefined;
-        solicitante?: mongoose.Types.ObjectId | null | undefined;
-        categoria?: string | null | undefined;
         partida?: mongoose.Types.ObjectId | null | undefined;
     } & mongoose.DefaultTimestampProps, {
         id: string;
@@ -59,12 +59,12 @@ export declare const DesafioSchema: mongoose.Schema<any, mongoose.Model<any, any
         collection: string;
     }>> & Omit<{
         jogadores: mongoose.Types.ObjectId[];
+        categoria?: string | null | undefined;
         dataHoraDesafio?: NativeDate | null | undefined;
+        solicitante?: mongoose.Types.ObjectId | null | undefined;
         status?: string | null | undefined;
         dataHoraSolicitacao?: NativeDate | null | undefined;
         dataHoraResposta?: NativeDate | null | undefined;
-        solicitante?: mongoose.Types.ObjectId | null | undefined;
-        categoria?: string | null | undefined;
         partida?: mongoose.Types.ObjectId | null | undefined;
     } & mongoose.DefaultTimestampProps & {
         _id: mongoose.Types.ObjectId;
@@ -75,12 +75,12 @@ export declare const DesafioSchema: mongoose.Schema<any, mongoose.Model<any, any
     }> | undefined;
 }, {
     jogadores: mongoose.Types.ObjectId[];
+    categoria?: string | null | undefined;
     dataHoraDesafio?: NativeDate | null | undefined;
+    solicitante?: mongoose.Types.ObjectId | null | undefined;
     status?: string | null | undefined;
     dataHoraSolicitacao?: NativeDate | null | undefined;
     dataHoraResposta?: NativeDate | null | undefined;
-    solicitante?: mongoose.Types.ObjectId | null | undefined;
-    categoria?: string | null | undefined;
     partida?: mongoose.Types.ObjectId | null | undefined;
     createdAt: NativeDate;
     updatedAt: NativeDate;
